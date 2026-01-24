dc6e42022da163c132df08d6dbbc4f98754b63f7
Binh.BuiDang <dangbinh4869@gmail.com>
2026-01-24 10:41:14 +0700

    add simple ui.

--- Sources/agentmonitor/agentmonitor.swift
+++ Sources/agentmonitor/agentmonitor.swift
@@ -1,9 +1,36 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import SwiftUI
+import AppKit
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationWillFinishLaunching(_ notification: Notification) {
+        NSApp.setActivationPolicy(.regular)
+    }
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        NSApp.activate(ignoringOtherApps: true)
+    }
+}
 
 @main
-struct agentmonitor {
-    static func main() {
-        print("Hello, world!")
+struct AgentMonitorApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, World!")
+                .font(.largeTitle)
+        }
+        .padding()
     }
 }
